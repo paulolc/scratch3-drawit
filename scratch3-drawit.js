e51eee8dfcca830443f4d9f967a51f95c4e19bb3
paulolc <plcarvalho@gmail.com>
2021-07-03 03:11:40 +0000

    initial version

---
+++ scratch3-drawit.js
@@ -0,0 +1,39 @@
+
+const readline = require('readline');
+const ScratchBridge = require('../scratch3-bridge');
+
+let msgrbridge = new ScratchBridge();
+//Alternative (no user prompts made):
+//let msgrbridge = new ScratchBridge( "ascratchuser", "theuserpassword",PROJECT_ID);
+
+
+
+msgrbridge.connect();
+let sender;
+
+msgrbridge.on('data', (msg,asender) =>{
+    console.log(`>(${asender}): ${msg}`);
+    sender = asender;
+    msgrbridge.send( sender, `String reversed on the server: ${msg.toString().split('').reverse().join('')}`  );
+});
+
+msgrbridge.on('connect', () => {
+    console.log( "Connected!! Sending your messages to Scratch:")
+    
+    const stdinput = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    stdinput.on('line', (msg) => {
+        
+        //msgrbridge.send( sender, `Message written on the server: ${msg}` );
+        msgrbridge.send( sender, msg.split(' ') );
+        if(msg === "quit"){
+            stdinput.close();
+        }
+    })
+
+})
+
+
